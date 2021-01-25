65104a51749da9e0f5397ae1ed70cced0adf7c78
takuya805 <takuya0903exile@gmail.com>
2021-01-25 06:05:20 +0000

    create bookmark function

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,9 @@ Rails.application.routes.draw do
     resources :reviews do
       resource :favorites, only: [:create, :destroy]
     end
+    resources :shops do
+      resource :bookmarks, only: [:create, :destroy]
+    end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
