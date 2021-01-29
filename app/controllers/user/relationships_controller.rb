1a1a61560e4b99b1b1e7427a4dc3d747e659e1da
takuya805 <takuya0903exile@gmail.com>
2021-01-29 00:57:14 +0000

    create relationship function

--- app/controllers/user/relationships_controller.rb
+++ app/controllers/user/relationships_controller.rb
@@ -1,11 +1,15 @@
 class User::RelationshipsController < ApplicationController
   def create
-    @follow = current_user.active_relationships.build(follower_id: params[:user_id])
+    @review = Review.find(params[:review_id])
+    @user = User.find(params[:user_id])
+    @follow = current_user.active_relationships.build(follower_id: @user.id)
     @follow.save
   end
 
   def destroy
-    @follow = current_user.active_relationships.find_by(follower_id: params[:user_id])
+    @review = Review.find(params[:review_id])
+    @user = User.find(params[:user_id])
+    @follow = current_user.active_relationships.find_by(follower_id: @user.id)
     @follow.destroy
   end
 end
