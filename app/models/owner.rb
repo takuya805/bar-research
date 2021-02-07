fae513f90f99c59c09534b74bfce49bd43b5b43a
takuya805 <takuya0903exile@gmail.com>
2021-02-07 06:12:35 +0000

    modified deproy

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -2,7 +2,7 @@ class Owner < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
+         :recoverable, :rememberable, :validatable #:confirmable
   has_many :shops, dependent: :destroy
   validates :name, presence: true
 
