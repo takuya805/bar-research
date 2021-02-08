eb7b1a3f19d95f4c2d7c77b2ea7f7028c1cfed84
takuya805 <takuya0903exile@gmail.com>
2021-02-08 07:17:28 +0000

    relationshiop update

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
+         :recoverable, :rememberable, :validatable #:confirmable
   attachment :image
   has_many :user_rooms, dependent: :destroy
   has_many :chats, dependent: :destroy
