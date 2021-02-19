e5210dcf04a02ce55cd7b5ae58bee4bcb8287aaa
takuya805 <takuya0903exile@gmail.com>
2021-02-19 06:44:20 +0000

    modify layout

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -8,4 +8,12 @@ class Tweet < ApplicationRecord
     favorites.where(user_id: user.id).exists?
   end
 
+  def avg_score
+    if comments.empty?
+      0.0
+    else
+     comments.average(:score).round(1).to_f
+    end
+  end
+
 end
