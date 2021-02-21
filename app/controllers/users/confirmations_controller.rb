4c3193cdccac2f71eebb23fe27eb412a4a863074
takuya805 <takuya0903exile@gmail.com>
2021-02-21 11:52:13 +0000

    devise_japanese

--- app/controllers/users/confirmations_controller.rb
+++ app/controllers/users/confirmations_controller.rb
@@ -16,15 +16,15 @@ class Users::ConfirmationsController < Devise::ConfirmationsController
   # end
 
   # GET /resource/confirmation?confirmation_token=abcdef
-   def show
-     self.resource = resource_class.find_by_confirmation_token(params[:confirmation_token])
+  # def show
+  #   self.resource = resource_class.find_by_confirmation_token(params[:confirmation_token])
 
-    if resource.nil? || resource.confirmed?
-      # トークンが不正な場合、アカウント登録(パスワード登録)が済んでいる場合
-      self.resource = resource_class.confirm_by_token(params[:confirmation_token])
-      resend_confirmation_instructions
-    end
-   end
+  #   if resource.nil? || resource.confirmed?
+  #     # トークンが不正な場合、アカウント登録(パスワード登録)が済んでいる場合
+  #     self.resource = resource_class.confirm_by_token(params[:confirmation_token])
+  #     resend_confirmation_instructions
+  #   end
+  # end
 
   # protected
 
--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     sessions: 'users/sessions',
     registrations: 'users/registrations',
-    omniauth_callbacks: 'users/omniauth_callbacks',
-    confirmations: 'users/confirmations'
+    omniauth_callbacks: 'users/omniauth_callbacks'
   }
   devise_scope :user do
     get "sign_in", :to => "users/sessions#new"
