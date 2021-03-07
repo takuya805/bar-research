96605185bc178ae84020188ac300cbc0dbde0fbb
takuya805 <takuya0903exile@gmail.com>
2021-03-07 10:05:46 +0000

    modify migration file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_06_145924) do
+ActiveRecord::Schema.define(version: 2021_03_06_140224) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
