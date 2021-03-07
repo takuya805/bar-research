f8446d4ccd4af7bb9476d809462dcb31f42cddf8
takuya805 <takuya0903exile@gmail.com>
2021-03-07 12:08:08 +0000

    add validation error

--- app/controllers/user/users_controller.rb
+++ app/controllers/user/users_controller.rb
@@ -6,7 +6,6 @@ class User::UsersController < ApplicationController
     @tweets = @user.tweets
     @plans  = current_user.plans
     @plan = Plan.new
-    @shops = Shop.all
     @user_contact = UserContact.new
   end
 
--- app/models/plan.rb
+++ app/models/plan.rb
@@ -1,3 +1,7 @@
 class Plan < ApplicationRecord
   belongs_to :user
+  with_options presence: true do
+   validates :shop_name
+   validates :start_time
+  end
 end
